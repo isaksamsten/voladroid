72fa19e2b9c3d5a4674707b6790caf5ff943ea2a
Isak Karlsson <isak.karlsson@gmail.com>
2012-01-25 10:43:09 +0100

    instance browser

--- src/com/voladroid/ui/ClassDialog.java
+++ src/com/voladroid/ui/ClassDialog.java
@@ -1,5 +1,8 @@
 package com.voladroid.ui;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.mat.SnapshotException;
 import org.eclipse.mat.snapshot.ISnapshot;
 import org.eclipse.mat.snapshot.model.FieldDescriptor;
@@ -7,6 +10,8 @@ import org.eclipse.mat.snapshot.model.IClass;
 import org.eclipse.mat.snapshot.model.IObject;
 import org.eclipse.mat.snapshot.model.NamedReference;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -16,7 +21,13 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
+import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.TableTree;
+import org.eclipse.swt.events.TreeAdapter;
+import org.eclipse.swt.events.TreeEvent;
 
 /**
  * This code was edited or generated using CloudGarden's Jigloo SWT/Swing GUI
@@ -32,10 +43,9 @@ public class ClassDialog extends org.eclipse.swt.widgets.Dialog {
 
 	private Shell dialogShell;
 	private IClass clazz;
-	private List instanceList;
-	private Table values;
-	private TableColumn tableColumn1;
-	private TableColumn tableColumn2;
+	private Tree tree1;
+	private TreeColumn valCol;
+	private TreeColumn keyCol;
 
 	/**
 	 * Auto-generated main method to display this org.eclipse.swt.widgets.Dialog
@@ -53,60 +63,47 @@ public class ClassDialog extends org.eclipse.swt.widgets.Dialog {
 			dialogShell = new Shell(parent, SWT.DIALOG_TRIM);
 
 			GridLayout dialogShellLayout = new GridLayout();
-			dialogShellLayout.numColumns = 2;
 			dialogShellLayout.makeColumnsEqualWidth = true;
 			dialogShell.setLayout(dialogShellLayout);
 			dialogShell.layout();
 			dialogShell.pack();
 			dialogShell.setSize(655, 403);
 			{
-				GridData list1LData = new GridData();
-				list1LData.grabExcessVerticalSpace = true;
-				list1LData.horizontalAlignment = GridData.FILL;
-				list1LData.verticalAlignment = GridData.FILL;
-				instanceList = new List(dialogShell, SWT.NONE);
-				instanceList.setLayoutData(list1LData);
-			}
-			{
-				GridData table1LData = new GridData();
-				table1LData.grabExcessHorizontalSpace = true;
-				table1LData.grabExcessVerticalSpace = true;
-				table1LData.horizontalAlignment = GridData.FILL;
-				table1LData.verticalAlignment = GridData.FILL;
-				values = new Table(dialogShell, SWT.NONE);
-				values.setLayoutData(table1LData);
-				values.setHeaderVisible(true);
+				GridData tree1LData = new GridData();
+				tree1LData.verticalAlignment = GridData.FILL;
+				tree1LData.horizontalAlignment = GridData.FILL;
+				tree1LData.grabExcessHorizontalSpace = true;
+				tree1LData.grabExcessVerticalSpace = true;
+				tree1 = new Tree(dialogShell, SWT.NONE);
+				tree1.setLayoutData(tree1LData);
+				tree1.setHeaderVisible(true);
+				tree1.addTreeListener(new TreeAdapter() {
+					public void treeExpanded(TreeEvent evt) {
+						tree1TreeExpanded(evt);
+					}
+				});
 				{
-					tableColumn1 = new TableColumn(values, SWT.NONE);
-					tableColumn1.setText("Variable");
-					tableColumn1.setWidth(60);
+					keyCol = new TreeColumn(tree1, SWT.NONE);
+					keyCol.setText("Key");
+					keyCol.setWidth(60);
 				}
 				{
-					tableColumn2 = new TableColumn(values, SWT.NONE);
-					tableColumn2.setText("Value");
-					tableColumn2.setWidth(139);
+					valCol = new TreeColumn(tree1, SWT.NONE);
+					valCol.setText("Value");
+					valCol.setWidth(60);
 				}
 			}
+
 			dialogShell.setLocation(getParent().toDisplay(100, 100));
 			dialogShell.open();
 			try {
 				for (int id : clazz.getObjectIds()) {
 					IObject obj = clazz.getSnapshot().getObject(id);
-					instanceList.add(obj.getDisplayName());
-					java.util.List<NamedReference> value = obj
-							.getOutboundReferences();
-					for (NamedReference n : value) {
-						System.out.println(n.getName());
-						System.out.println(clazz.getSnapshot()
-								.getObject(n.getObjectId())
-								.getClassSpecificName());
-					}
-					for (FieldDescriptor d : clazz.getFieldDescriptors()) {
 
-						TableItem item = new TableItem(values, SWT.NONE);
-						item.setText(0, d.getName());
-						item.setText(1, d.getVerboseSignature());
-					}
+					TreeItem item = new TreeItem(tree1, SWT.NONE);
+					item.setText(obj.getDisplayName());
+					item.setData(obj);
+					item.setItemCount(1);
 				}
 			} catch (SnapshotException e) {
 				// TODO Auto-generated catch block
@@ -119,6 +116,52 @@ public class ClassDialog extends org.eclipse.swt.widgets.Dialog {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	private void tree1TreeExpanded(TreeEvent evt) {
+		try {
+			TreeItem item = (TreeItem) evt.item;
+			IObject obj = (IObject) item.getData();
+			item.removeAll();
+
+			populate(item, obj);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param item
+	 * @param obj
+	 * @throws SnapshotException
+	 */
+	protected void populate(TreeItem item, IObject obj)
+			throws SnapshotException {
+		java.util.List<NamedReference> values = obj.getOutboundReferences();
+		Set<String> used = new HashSet<String>();
+		for (NamedReference n : values) {
+			TreeItem c = new TreeItem(item, SWT.NONE);
+			c.setText(0, n.getName());
+			String clsSpec = n.getObject().getClassSpecificName();
+			if (clsSpec != null) {
+				c.setText(1, clsSpec);
+			} else {
+				c.setData(n.getObject());
+				c.setText(1, n.getObject().getDisplayName());
+				c.setItemCount(1);
+			}
+			used.add(n.getName());
+		}
+		for (FieldDescriptor d : clazz.getFieldDescriptors()) {
+			if (used.contains(d.getName()))
+				continue;
+			Object value = obj.resolveValue(d.getName());
+			if (value != null) {
+				TreeItem c = new TreeItem(item, SWT.NONE);
+				c.setText(0, d.getName());
+				c.setText(1, value.toString());
+			}
 		}
 	}
 
